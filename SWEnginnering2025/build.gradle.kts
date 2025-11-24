edd4ca3e939316d8efafa0ccab1a46b9411f9065
DESKTOP-FI1A4IU\이채민 <coals0708@gmail.com>
2025-11-25 00:30:36 +0900

    H2서버 관련 설정추가 (Security.java)

--- SWEnginnering2025/build.gradle.kts
+++ SWEnginnering2025/build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	compileOnly("org.projectlombok:lombok")
